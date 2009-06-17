f54e58f45e86fdaa3f50c086bedb55ac57ca0053
dmichael <dmichael@ornoporno.local>
2009-06-17 14:38:16 -0400

    geometry.rb

--- geometry.rb
+++ geometry.rb
@@ -34,3 +34,73 @@ geometry.shapes.each do |shape|
   puts ''
 end
 
+=begin
+polygon-polygon intersect
+
+The edges are line segments, so you have two sets of line segments, one
+for each poly. If any line segment in one of the sets crosses any of the
+line segments in the other set, then the polygons intersect. Otherwise
+they don't.
+
+So:
+
+foreach a in A:
+foreach b in B:
+if a intersects b: return true
+return false
+
+is your basic program
+
+Then all you have to worry about is how to intersect two linesegments,
+which isn't too hard.
+
+Some of the algorithms for convex polygons are more sophisticated,
+basically because you can do some clever optimizations on this nested
+loop based on the fact that they're convex.
+
+
+
+
+http://local.wasp.uwa.edu.au/~pbourke/geometry/insidepoly/
+http://www.dfanning.com/tips/point_in_polygon.html
+
+
+
+bool PolyPolyIntersection( Polygon PolyA, Polygon PolyB )
+{
+	if( !Poly[ A ].size() || !Poly[ B ].size() )
+		return false; //Be sure that these polygons actually have verticies on them
+
+	//Check each segment on polyB for intersection with each segment of PolyB
+	int pA0 = polyA.size() - 1;
+	int pA1 = 0;
+	do
+	{
+
+		int pB0 = polyB.size() - 1;
+		int pB1 = 0;
+		do
+		{
+			if( SegmentSegmentIntersection( polyA[ pA0 ], polyA[ pA1 ], polyB[ pB0 ], polyB[ pB1 ] ) )
+				return true;
+
+			pB0 = pB1;
+			pB1++;
+		}
+		while( pB1 < polyB.size() );
+
+
+		pA0 = pA1;
+		pA1++;
+	}
+	while( pA1 < polyA.size() );
+
+	//Determine if one polygon lies entierly within the other
+	if( PointInPoly( polyA[ 0 ], polyB || PointInPoly( polyB[ 0 ], polyA )
+		return true;
+
+	return false;
+}
+
+=end
+
